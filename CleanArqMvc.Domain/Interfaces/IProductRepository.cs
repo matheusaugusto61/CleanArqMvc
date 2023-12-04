d45f3c32d3fb0a8dab98ca6fb76db57497a8478e
Matheus Augusto Alves de Souza <matheus.augusto@animaeducacao.com.br>
2023-12-04 15:36:35 -0300

    Criando domain e application

---
+++ CleanArqMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int? id);
+        Task<Product> GetProductCategoryAsync(int? id);
+        Task<Product> CreateAsync(Product Product);
+        Task<Product> UpdateAsync(Product Product);
+        Task<Product> RemoveAsync(Product Product);
+    }
+}
